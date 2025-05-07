3099f925e2d2c8903889f4c404f1a3fddbe6abea
csirvent21 <carlos@sirvent.info>
2025-05-06 22:58:35 -0700

    Add files via upload

---
+++ Lab 11/main.rb
@@ -0,0 +1,30 @@
+array=[ 1, 
+  2, 
+  3,
+  [4, 5, 6],
+  7,
+  [8,
+    [9, 10, 11,
+      [12, 13, 14]
+    ] 
+  ],
+  [15, 16, 17, 18, 19,
+    [20, 21, 22,
+      [23, 24, 25,
+        [26, 27, 29]
+      ], 30, 31 
+    ], 32
+  ], 33 
+]
+
+def printnumbers(n)
+  n.each do |i|
+    if i.is_a? (Array)
+      printnumbers(i)
+    else
+      puts i
+      end
+    end
+  end
+
+printnumbers(array)
